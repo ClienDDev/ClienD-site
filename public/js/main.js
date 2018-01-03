241b8a673fcd77e1cc5d3515b9039eab2497b96e
atnartur <i@atnartur.ru>
2018-01-03 19:20:06 +0300

    + js & images

---
+++ public/js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $('#loginform').submit(function(e){
+        e.preventDefault();
+        const $this = $(this);
+        const login = $this.find('#login').val();
+        const pass = $this.find('#pass').val();
+        $this.find('button').text('Происходит вход...');
+
+        const url = 'http://app.cliend.ru';
+        const w = window.open(url);
+
+        console.log(w);
+        $(w).ready(function() {
+            setTimeout(function() {
+                console.log('app loaded');
+                w.postMessage({cliendLogin: {login: login, pass: pass}}, url);
+                w.focus();
+                window.close();
+            }, 500);
+        });
+    })
+});
