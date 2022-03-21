4f92f90d9118a5d4a90b3237fe3a7737d10bee44
atnartur <i@atnartur.ru>
2022-03-21 23:32:17 +0300

    + страница прекращения поддержки

--- gulp/server.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const serve = require('gulp-serve');
-const config = require('./config');
-
-gulp.task('server', serve({
-    root: config.public,
-    port: 3000
-}));
